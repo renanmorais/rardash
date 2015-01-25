403d1dfb44fab5037f1bcf660f8f555f3cdb59eb
Rodnei A. Ribeiro <rodnei.solucoes@gmail.com>
2015-01-25 00:34:05 -0200

    Fix check and uncheck datatable pagination

--- app/scripts/util/checkAll.js
+++ app/scripts/util/checkAll.js
@@ -28,3 +28,5 @@ checkElements.checkAll = function (event){
   }
 
 };
+
+
--- app/scripts/vendor_config/datatable.js
+++ app/scripts/vendor_config/datatable.js
@@ -14,7 +14,23 @@ datatable.generateTable = function(_element, _type, _urlTranslateTable, _data){
     responsive: true,
     columnDefs: [
       { targets: [0], orderable: false }
-    ]
+    ],
+    'fnDrawCallback': function() {
+      /**
+       * Remove all check in datatable
+       */
+      var getElementsRemove = document.querySelectorAll('.elementCheck');
+      var countElementsRemove = getElementsRemove.length;
+
+      while(countElementsRemove--){
+        getElementsRemove[countElementsRemove].checked = false;
+      }
+      var getActionsRemove = document.querySelector('.action-table');
+      getActionsRemove.style.display = 'none';
+
+      var getElementAction = document.getElementById('allCheck');
+      getElementAction.checked = false;
+    }
   });
 
 };
